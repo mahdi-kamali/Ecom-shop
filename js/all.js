e524a914ce7b8f8633289bdff18209d4eab9cf11
Wotox2 <MAHDIKAMALY20@GMAIL.COM>
2022-09-13 13:36:45 +0430

    Project Done

--- js/all.js
+++ js/all.js
@@ -14,3 +14,22 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
     },
 });
+
+
+const imagePicker = document.getElementById('image-picker')
+const imageElement = document.getElementById('image-element')
+
+imagePicker.onchange = (event) => {
+    const file = event.target.files[0]
+    console.log(event.target.files[0]);
+
+    const fileReader = new FileReader()
+
+    fileReader.onload = (e) => {
+        const src = e.target.result
+        imageElement.src = src
+    }
+
+    fileReader.readAsDataURL(file)
+
+}
